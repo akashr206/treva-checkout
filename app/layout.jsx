92e4567d75e49498b990e5a02a716156ba790351
akashr206 <akashr6514@gmail.com>
2025-05-17 10:27:14 +0530

    Initial commit from Create Next App

---
+++ app/layout.jsx
@@ -0,0 +1,24 @@
+import { Geist } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Treva",
+  description: "Treva assignment",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
